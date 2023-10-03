39d0943a2a0213faec40473e2cbc99c418266ce6
Eduardo-Hoffmann <123705923+Eduardo-Hoffmann@users.noreply.github.com>
2023-10-02 22:14:55 -0300

    faltaConclusao

---
+++ index.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np  
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score, confusion_matrix
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("./dados_produtos.csv")
+
+print(data.isna())
+print(data.isnull())
+
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='product_name', y='rating', data=data)
+plt.xlabel('Produto')
+plt.ylabel('Rating')
+plt.show()
+
+
+
+trainData, testeData = train_test_split(data, test_size=0.2, random_state=42)
+
+produtos = data['product_name'].unique()
+
+for produto in produtos:
+    dados_produto = trainData[trainData['product_name'] == produto]
+    
+    X = dados_produto[['rating', 'rating_count']]
+    y = dados_produto['purchased']
+    
+    modelo = DecisionTreeClassifier(random_state=42)
+    modelo.fit(X, y)
+    
+    dados_teste_produto = testeData[testeData['product_name'] == produto]
+    
+    X_teste = dados_teste_produto[['rating', 'rating_count']]
+    y_teste = dados_teste_produto['purchased']
+    
+    y_pred = modelo.predict(X_teste)
+
+    plt.figure(figsize=(12, 8))
+    plot_tree(modelo, filled=True, feature_names=['rating', 'rating_count'], class_names=['Not Purchased', 'Purchased'])
+    plt.title(f'Árvore de Decisão para {produto}')
+    plt.show()
+
+
+#>>>>> Conclusão <<<<<
+#
+
+
